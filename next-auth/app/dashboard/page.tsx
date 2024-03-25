8bc29f07ddee4cca5f2c93ca0efbc9a9c29744b0
annamarcomini <annajuliamarcomini23@gmail.com>
2024-03-24 22:21:06 -0300

    utilizando o contexto para exibir o email do user

--- next-auth/app/dashboard/page.tsx
+++ next-auth/app/dashboard/page.tsx
@@ -1,11 +1,16 @@
+"use client"
+import { useContext } from "react"
+import { AuthContext } from "@/contexts/AuthContext"
 import styles from "./page.module.css"
 
-export default function Home() {
+export default function Dashboard() {
+  // Verifica se o código está sendo executado do lado do cliente antes de usar o contexto
+
+  const {user} = useContext(AuthContext)
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1> helloooo</h1>
-      </div>
-    </main>
+    <div className={styles.description}>
+      <h1>Dashboard: {user?.email}</h1>
+    </div>
   )
 }
