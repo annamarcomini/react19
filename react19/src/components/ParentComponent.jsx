c04256e08970c5177f3f2ecebd86bf9879d0c0a1
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-10 15:29:30 -0300

    feat: parent component creation to test hook use

---
+++ react19/src/components/ParentComponent.jsx
@@ -0,0 +1,11 @@
+import {useRef, useEffect} from "react"
+
+const ParentComponent = () => {
+ const inputRef = useRef(null);
+
+  
+ return (
+  <div>Parent Component</div>
+ )
+};
+export default ParentComponent
