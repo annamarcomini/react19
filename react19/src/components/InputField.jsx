b810815747cf0d02e65ed91c4c6dd0bb765a1e6b
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-10 15:38:58 -0300

    feat: Input component creation & props creation

---
+++ react19/src/components/InputField.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputField = () => {
+ return <input type="text" placeholder={placeholder} ref={ref}></input> //{} text between is a props
+};
+
+export default InputField;
