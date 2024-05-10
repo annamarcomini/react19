056c18a25dcb16c5115e3297d3947d1ec27f6cdb
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-10 14:38:54 -0300

    feat: user form component creation

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      
+      <h1>1 - Actions</h1>
     </>
   )
 }
---
+++ react19/src/components/UserForm.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+const UserForm = () => {
+ const [users, setUsers] = useState([]);
+
+
+
+ 
+ return (
+  <div>UserForm</div>
+ )
+}
+
+export default Userform
