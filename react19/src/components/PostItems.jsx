d162324fc20d4779ac1eecc7ea28a2ef712870ce
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-10 17:13:31 -0300

    feat: consuming data with hook use

--- react19/src/components/PostItems.jsx
+++ react19/src/components/PostItems.jsx
@@ -1,10 +1,20 @@
 import {use} from "react"
 
+const fetchPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  return response.json()
+}
 
 
 const PostItems = () => {
+ const posts = use(fetchPosts());
 
  return <div>
+  <ul>
+   {posts.map((post) => (
+    <li key={post.id}>{post.title}</li>
+   ))}
+  </ul>
   
   </div>
 }
