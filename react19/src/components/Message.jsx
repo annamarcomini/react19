6f2bb5894382f753f561030fbbb00e9968c7e835
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-12 20:44:55 -0300

    feat: message send functions

--- react19/src/components/Message.jsx
+++ react19/src/components/Message.jsx
@@ -5,9 +5,21 @@ export default function Message() {
  const [messages, setMessages] = useState([
   {text: "Hi, your first message", sending: false, key: 1},
  ])
+ async function deliveryMessage(message) {
+
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  
+  return message;
+ }
+
+ async function sendMessage(formData) {
+  const sentMessage = await deliveryMessage(formData.get("message"))
+  
+  setMessages((messages) => [...messages, { text: sentMessage, sending: false }]);
+ }
  return (
    <div>
-     <MessageForm messages={messages} />;
+   <MessageForm messages={messages} sendMessage={sendMessage } />
    </div>
  )
 }
