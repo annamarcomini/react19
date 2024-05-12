0d3b5ef73702f2d98d12485294a366e19ca34d37
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-12 20:06:27 -0300

    feat: state creation

--- react19/src/components/Message.jsx
+++ react19/src/components/Message.jsx
@@ -1,8 +1,13 @@
-
+import { useState } from "react"
+import MessageForm from "./MessageForm"
 
 export default function Message() {
- 
+ const [messages, setMessages] = useState([
+  {text: "Hi, your first message", sending: false, key: 1},
+ ])
  return (
-  
+   <div>
+     <MessageForm messages={messages} />;
+   </div>
  )
 }
