beb735d195230b122fb2f712c438cc5d1d82a8f5
annamarcomini <annajuliamarcomini23@gmail.com>
2024-05-12 16:59:43 -0300

    feat: success validation based on form state

--- react19/src/components/FormState.jsx
+++ react19/src/components/FormState.jsx
@@ -1,11 +1,21 @@
+import { useFormState } from 'react-dom';
 
 export default function FormStatus() {
-
  
+ const validateUser = () => {
+  
+ };
+
+ const [message, formAction] = useFormState(validateUser, null);
  
  return (
   <div>
-   
+   <form action={formAction}>
+    <label htmlFor="userName">Name</label>
+    <input type="text" name="userName" />
+    <button type="submit"></button>
+    {message && <h4>{message.text}</h4>}
+   </form>
   </div>
  )
 }
